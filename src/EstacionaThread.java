ce8d7bad6efeb80b07cec8f0038e50559cc0655a
matheus <matheus.frndes@gmail.com>
2014-07-06 19:25:27 -0300

    Final

--- src/EstacionaThread.java
+++ src/EstacionaThread.java
@@ -1,28 +1,38 @@
+import java.util.Random;
+
 /**
  * Created by matheus on 06/07/14.
  */
     public class EstacionaThread extends Thread{
     private Estacionamento estacionamento;
     private int espera;
+    private int vezes;
     private int vaga;
 
-    public EstacionaThread(Estacionamento e) {
+    public EstacionaThread(Estacionamento e, int vezes) {
         super();
         this.estacionamento = e;
+        this.vezes = vezes;
     }
-    public EstacionaThread(Estacionamento e, String nome) {
+    public EstacionaThread(Estacionamento e, String nome, int vezes) {
         super(nome);
         this.estacionamento = e;
+        this.vezes = vezes;
     }
 
     public void run() {
         try {
-            vaga = estacionamento.estacionar();
-            System.out.println("Carro " + this.getName() + " estacionou na vaga " + vaga);
-            espera = (int) (3 * 1000);
-            sleep(espera);
-            estacionamento.sair(vaga);
-            System.out.println("Carro " + this.getName() + " saiu da vaga " + vaga);
+            int i = 0;
+            while(i < vezes) {
+                vaga = estacionamento.estacionar();
+                System.out.println("Carro " + this.getName() + " estacionou na vaga " + (vaga + 1));
+                espera = Math.abs(new Random().nextInt() % 10) * 1000;
+                sleep(espera);
+                estacionamento.sair(vaga);
+                System.out.println("Carro " + this.getName() + " saiu da vaga " + (vaga + 1));
+                ++i;
+            }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.util.Random;
+
 /**
  * Created by matheus on 06/07/14.
  */
 public class Main {
 
+    private static final int nCarros = 6;
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         Estacionamento estacionamento = new Estacionamento();
-        EstacionaThread carro1 = new EstacionaThread(estacionamento, "1");
-        carro1.start();
+        EstacionaThread[] carros = new EstacionaThread[nCarros];
+
+        for(int i = 0; i < nCarros; i ++) {
+            System.out.print(random.nextInt(4) + " ");
+        }
+
+        for(int i = 0; i < nCarros; i ++) {
+            carros[i] = new EstacionaThread(estacionamento, Integer.toString(i + 1), random.nextInt(4));
+            carros[i].start();
+        }
     }
 }
